ba5469a0c2dcd14598561b599c5112fb7144bcb1
Nawa Manusitthipol <nawa@nawaman.net>
2019-09-08 00:38:33 -0400

    Use v0.2.1.0

--- src/main/java/use/functionalj/maven/TryChoice.java
+++ src/main/java/use/functionalj/maven/TryChoice.java
@@ -1,7 +1,19 @@
 package use.functionalj.maven;
 
+import static functionalj.function.Func.f;
+import static functionalj.stream.StreamPlus.range;
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
+import static use.functionalj.maven.Tree.theTree;
+
 import functionalj.types.Choice;
+import functionalj.types.Nullable;
+import functionalj.types.Type;
 import functionalj.types.choice.Self;
+import functionalj.types.choice.generator.model.Case;
+import functionalj.types.choice.generator.model.CaseParam;
+import functionalj.types.choice.generator.model.SourceSpec;
+import lombok.val;
 
 public class TryChoice {
     
@@ -37,14 +49,14 @@ public class TryChoice {
     
     @Choice
     interface LoginStatusSpec {
-        void Login(String userName);
+        void Login(@Nullable String userName);
         void Logout();
     }
     
     @Choice
     interface ReqestResultSpec<T> {
         void Success(T data);
-        void Error(int error, String errorMessage);
+        void Error(String errorMessage);
         void ConnectionFailed(Exception problem);
     }
     
@@ -73,6 +85,39 @@ public class TryChoice {
                     .celsius   (c -> c)
                     .fahrenheit(f -> Temperature.Celsius((f.fahrenheit() - 32.0)/1.8));
         }
+    }
+    
+    public static final SourceSpec spec = new SourceSpec(
+            "Command",
+            new Type("use.functionalj.gradle", "TryChoice", "CommandSpec", emptyList()),
+            "spec",
+            false,
+            null,
+            emptyList(),
+            asList(
+                new Case("Forward", null, asList(
+                        new CaseParam("distance", new Type(null, null, "int", emptyList()), true, null))),
+                new Case("Backward", null, asList(
+                        new CaseParam("distance", new Type(null, null, "int", emptyList()), true, null))), 
+                new Case("Turn", null, asList(
+                        new CaseParam("angle", new Type(null, null, "int", emptyList()), true, null))), 
+                new Case("Explode", null, emptyList())), 
+            emptyList(), 
+            emptyList());
+    
+    public static void main(String[] args) {
+        val tree1 = Tree.Node("2", Tree.Leaf("2"), Tree.Leaf("3"));
+        val is2 = theTree.asNode.get().value.thatEquals("2")
+              .or(theTree.asLeaf.get().value.thatEquals("2"));
+        
+        System.out.println(is2.apply(tree1));
+        System.out.println(is2.apply(tree1.left()));
+        System.out.println(is2.apply(tree1.right()));
+        
+        System.out.println(range(1, 6).reduce((a,b)->a*b));
+        
+        val factorial = f((Integer n) -> range(1, n + 1).reduce((a,b)->a*b).orElse(1));
+        System.out.println(factorial.apply(5));
     }
     
 }
