9df7a9e195d90c3a6cc39da7550e66b62a8b48c7
Nawa Manusitthipol <nawa@nawaman.net>
2019-09-09 01:04:23 -0400

    Let's put some test on example project.

---
+++ src/test/java/use/functionalj/maven/list/FuncListTest.java
@@ -0,0 +1,34 @@
+package use.functionalj.maven.list;
+import static org.junit.Assert.*;
+import static use.functionalj.maven.list.Employee.theEmployee;
+
+import java.time.LocalDate;
+
+import org.junit.Test;
+
+import functionalj.list.FuncList;
+import lombok.val;
+import use.functionalj.maven.list.Employee;
+
+public class FuncListTest {
+
+    @Test
+    public void test() {        val employees = FuncList.of(
+            new Employee("Kailyn Kerenza", LocalDate.of(1970,  8, 10), 59000),
+            new Employee("Randi Kathlyn",  LocalDate.of(1971, 10, 13), 43000),
+            new Employee("Layla Darrin",   LocalDate.of(1976,  2, 25), 63000),
+            new Employee("Bambi Vianne",   LocalDate.of(1998,  7, 20), 64000),
+            new Employee("Lila Rowina",    LocalDate.of(1999,  3, 22), 82000)
+        );
+        
+        val Jan_1st_2019 = LocalDate.of(2019, 1, 1);
+        double averageCompensationOfOldEmplyees = employees
+                .filter  (theEmployee.birthday.until(Jan_1st_2019).getYears().thatGreaterThan(40))
+                .mapToInt(theEmployee.compensation)
+                .average()
+                .orElse(0.0);
+        
+        assertEquals("55000.0", "" + averageCompensationOfOldEmplyees);
+    }
+
+}
--- src/test/java/use/functionalk/maven/MonadAgnosticTest.java
+++ src/test/java/use/functionalk/maven/MonadAgnosticTest.java
@@ -1,18 +1,22 @@
-package use.functionalj.maven;
+package use.functionalk.maven;
 
 import static functionalj.function.Func.f;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 import functionalj.function.Func2;
 import functionalj.result.Result;
 
-public class Comprehension {
+public class MonadAgnosticTest {
     
-    public static void main(String[] args) {
+    @Test
+    public void testMonadAgnostic() {
         Func2<String, String, String> func = f((s1, s2)->s1+" "+s2);
         
         Result<String> text1 = Result.valueOf("Text1");
         Result<String> text2 = Result.valueOf("Text2");
-        System.out.println(func.applyTo(text1, text2));     //  "Result:{ Value: Text1 Text2 }"
+        assertEquals("Result:{ Value: Text1 Text2 }", "" + func.applyTo(text1, text2));
     }
     
 }
