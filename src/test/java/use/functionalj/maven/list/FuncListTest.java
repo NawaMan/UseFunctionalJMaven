7e9ac5c1c39722d8560af562cb26bb4294773f43
NawaMan <nawa@nawaman.net>
2021-06-29 10:38:09 -0400

    Modernize to Java11 to jUnit5.

--- src/test/java/use/functionalj/maven/list/FuncListTest.java
+++ src/test/java/use/functionalj/maven/list/FuncListTest.java
@@ -1,4 +1,5 @@
 package use.functionalj.maven.list;
+
 import static org.junit.Assert.assertEquals;
 import static use.functionalj.maven.list.Employee.theEmployee;
 
@@ -7,12 +8,12 @@ import java.time.LocalDate;
 import org.junit.Test;
 
 import functionalj.list.FuncList;
-import lombok.val;
 
-public class FuncListTest {
-
+class FuncListTest {
+    
     @Test
-    public void test() {        val employees = FuncList.of(
+    void test() {
+        var employees = FuncList.of(
             new Employee("Kailyn Kerenza", LocalDate.of(1970,  8, 10), 59000),
             new Employee("Randi Kathlyn",  LocalDate.of(1971, 10, 13), 43000),
             new Employee("Layla Darrin",   LocalDate.of(1976,  2, 25), 63000),
@@ -20,9 +21,10 @@ public class FuncListTest {
             new Employee("Lila Rowina",    LocalDate.of(1999,  3, 22), 82000)
         );
         
-        val Jan_1st_2019 = LocalDate.of(2019, 1, 1);
-        double averageCompensationOfOldEmplyees = employees
-                .filter  (theEmployee.birthday.until(Jan_1st_2019).getYears().thatGreaterThan(40))
+        var Jan_1st_2019 = LocalDate.of(2019, 1, 1);
+        var oldAge       = 40;
+        var averageCompensationOfOldEmplyees = employees
+                .filter  (theEmployee.birthday.until(Jan_1st_2019).getYears().thatGreaterThan(oldAge))
                 .mapToInt(theEmployee.compensation)
                 .average()
                 .orElse(0.0);
--- src/test/java/use/functionalk/maven/MonadAgnosticTest.java
+++ src/test/java/use/functionalk/maven/MonadAgnosticTest.java
@@ -5,17 +5,16 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
 
-import functionalj.function.Func2;
 import functionalj.result.Result;
 
-public class MonadAgnosticTest {
+class MonadAgnosticTest {
     
     @Test
-    public void testMonadAgnostic() {
-        Func2<String, String, String> func = f((s1, s2)->s1+" "+s2);
+    void testMonadAgnostic() {
+        var func = f((String s1, String s2) -> s1+" "+s2);
         
-        Result<String> text1 = Result.valueOf("Text1");
-        Result<String> text2 = Result.valueOf("Text2");
+        var text1 = Result.valueOf("Text1");
+        var text2 = Result.valueOf("Text2");
         assertEquals("Result:{ Value: Text1 Text2 }", "" + func.applyTo(text1, text2));
     }
     
