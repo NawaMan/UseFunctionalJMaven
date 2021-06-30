7586a2084190663c27394bfd717c003fadb06356
NawaMan <nawa@nawaman.net>
2021-06-30 10:04:44 -0400

    Make jUnit 5 works.

--- src/test/java/use/functionalj/maven/list/FuncListTest.java
+++ src/test/java/use/functionalj/maven/list/FuncListTest.java
@@ -1,35 +1,35 @@
 package use.functionalj.maven.list;
-//
-//import static org.junit.jupiter.api.Assertions.assertEquals;
-//import static use.functionalj.maven.list.Employee.theEmployee;
-//
-//import java.time.LocalDate;
-//
-//import org.junit.jupiter.api.Test;
-//
-//import functionalj.list.FuncList;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static use.functionalj.maven.list.Employee.theEmployee;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import functionalj.list.FuncList;
 
 class FuncListTest {
-//    
-//    @Test
-//    void test() {
-//        var employees = FuncList.of(
-//            new Employee("Kailyn Kerenza", LocalDate.of(1970,  8, 10), 59000),
-//            new Employee("Randi Kathlyn",  LocalDate.of(1971, 10, 13), 43000),
-//            new Employee("Layla Darrin",   LocalDate.of(1976,  2, 25), 63000),
-//            new Employee("Bambi Vianne",   LocalDate.of(1998,  7, 20), 64000),
-//            new Employee("Lila Rowina",    LocalDate.of(1999,  3, 22), 82000)
-//        );
-//        
-//        var Jan_1st_2019 = LocalDate.of(2019, 1, 1);
-//        var oldAge       = 40;
-//        var averageCompensationOfOldEmplyees = employees
-//                .filter  (theEmployee.birthday.until(Jan_1st_2019).getYears().thatGreaterThan(oldAge))
-//                .mapToInt(theEmployee.compensation)
-//                .average()
-//                .orElse(0.0);
-//        
-//        assertEquals("55000.0", "" + averageCompensationOfOldEmplyees);
-//    }
+    
+    @Test
+    void test() {
+        var employees = FuncList.of(
+            new Employee("Kailyn Kerenza", LocalDate.of(1970,  8, 10), 59000),
+            new Employee("Randi Kathlyn",  LocalDate.of(1971, 10, 13), 43000),
+            new Employee("Layla Darrin",   LocalDate.of(1976,  2, 25), 63000),
+            new Employee("Bambi Vianne",   LocalDate.of(1998,  7, 20), 64000),
+            new Employee("Lila Rowina",    LocalDate.of(1999,  3, 22), 82000)
+        );
+        
+        var Jan_1st_2019 = LocalDate.of(2019, 1, 1);
+        var oldAge       = 40;
+        var averageCompensationOfOldEmplyees = employees
+                .filter  (theEmployee.birthday.until(Jan_1st_2019).getYears().thatGreaterThan(oldAge))
+                .mapToInt(theEmployee.compensation)
+                .average()
+                .orElse(0.0);
+        
+        assertEquals("55000.0", "" + averageCompensationOfOldEmplyees);
+    }
 
 }
--- src/test/java/use/functionalk/maven/MonadAgnosticTest.java
+++ src/test/java/use/functionalk/maven/MonadAgnosticTest.java
@@ -8,14 +8,14 @@ import org.junit.jupiter.api.Test;
 import functionalj.result.Result;
 
 class MonadAgnosticTest {
-//    
-//    @Test
-//    void testMonadAgnostic() {
-//        var func = f((String s1, String s2) -> s1+" "+s2);
-//        
-//        var text1 = Result.valueOf("Text1");
-//        var text2 = Result.valueOf("Text2");
-//        assertEquals("Result:{ Value: Text1 Text2 }", "" + func.applyTo(text1, text2));
-//    }
-//    
+    
+    @Test
+    void testMonadAgnostic() {
+        var func = f((String s1, String s2) -> s1+" "+s2);
+        
+        var text1 = Result.valueOf("Text1");
+        var text2 = Result.valueOf("Text2");
+        assertEquals("Result:{ Value: Text1 Text2 }", "" + func.applyTo(text1, text2));
+    }
+    
 }
